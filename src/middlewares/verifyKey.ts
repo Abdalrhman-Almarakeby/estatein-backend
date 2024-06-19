8650f5f9ad95eb8d09e51536557115b62aefcba8
Abdalrhman Almarakeby <aalmarakeby@gmail.com>
2024-06-19 06:04:40 +0300

    Update verifyAPIKey middleware.

--- src/middlewares/verifyKey.ts
+++ src/middlewares/verifyKey.ts
@@ -1,11 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
-const { API_SECRET_KEY } = process.env;
-
 export function verifyAPIKey(req: Request, res: Response, next: NextFunction) {
   const providedKey = req.headers["x-api-key"] as string;
 
-  if (!providedKey || providedKey !== API_SECRET_KEY) {
+  if (!providedKey || providedKey !== process.env.API_SECRET_KEY) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
